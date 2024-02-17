78ca77569e328c647422f6675f20a1c756acdc07
Oğuzhan Varlı <106310128+oguzhanvarli@users.noreply.github.com>
2024-02-17 14:41:22 +0300

    17 şubat gün sonu

--- first/src/screens/home/Home.js
+++ first/src/screens/home/Home.js
@@ -1,25 +1,50 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import { Button, FlatList, StyleSheet, Text, View, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Card } from '@rneui/themed';
 
 const Home = () => {
 
+  const [products, setProducts] = useState([])
+
   useEffect(() => {
     getProducts()
-  },[])
-  
+  }, [])
 
-  const getProducts = async() => {
+
+  const getProducts = async () => {
     try {
       let response = await axios.get('https://dummyjson.com/products').then(res => res.data)
       console.log(response)
+      setProducts(response.products)
     } catch (error) {
       console.log(error)
     }
   }
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flexDirection: 'row' }}>
+
+      <FlatList
+        data={products}
+        renderItem={({ item }) =>
+          <>
+            <Card.Title>{item.title}</Card.Title>
+            <View style={{ position: "relative", alignItems: "center",marginBottom: 15 }}>
+              <Image
+                style={{ width: 150, minWidth: 150, height: 100 }}
+                resizeMode="contain"
+                source={{ uri: item.thumbnail }}
+              />
+              <Text style={styles.price} >{item.price}$</Text>
+              <Button title="Add to Cart" onPress={null}  color='red'/>
+            </View>
+            <Card.Divider />
+          </>}
+        // horizontal
+        ListHeaderComponent={<Text style={{ color: 'red' }}>Oğuzhanın Hikayeleri bölümü</Text>}
+        ListFooterComponent={<Text style={{ color: 'red' }}>Burası son Taraf</Text>}
+      />
+
       {/* <Button onPress={getProducts} title="Get Products" /> */}
     </View>
   )
@@ -27,4 +52,10 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  price: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10
+  }
+})
